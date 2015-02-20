0cbf0715397c1449b092a59d15814c2b77267efd
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 07:51:26 -0800

    added db creation and remove on set up and tear down tests

--- vagrant/tradyfit/tests/test_basic.py
+++ vagrant/tradyfit/tests/test_basic.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import current_app
-from app import create_app
+from app import create_app, db
 
 
 class BasicsTestCase(unittest.TestCase):
@@ -8,8 +8,11 @@ class BasicsTestCase(unittest.TestCase):
         self.app = create_app('testing')
         self.app_context = self.app.app_context()
         self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
+        db.session.remove()
+        db.drop_all()
         self.app_context.pop()
 
     def test_app_exists(self):
