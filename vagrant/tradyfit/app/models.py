449c960c2a46003e88ed4fbce8293701902e2866
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 11:08:54 -0800

    added Category and Item models

---
+++ vagrant/tradyfit/app/models.py
@@ -0,0 +1,34 @@
+from . import db
+from datetime import datetime
+import os
+
+
+class Category(db.Model):
+	__tablename__ = 'categories'
+	id    = db.Column(db.Integer, primary_key=True)
+	name  = db.Column(db.String, unique=True, index=True)
+	items = db.relationship('Item', backref='category') 
+
+	@staticmethod
+	def insert_categories():
+		path = os.path.dirname(os.path.abspath(__file__))
+		f = os.path.join(path,"files/categories.txt")
+		with open(f, "r") as categories:
+			for category in categories: #read each line
+				category = category.rstrip('\n')
+				c = Category.query.filter_by(name=category).first()
+				if c is None: #not in db yet
+					category = Category(name=category)
+					db.session.add(category)
+		db.session.commit()
+
+
+class Item(db.Model):
+	__tablename__ = 'items'
+	id    = db.Column(db.Integer, primary_key=True)
+	name  = db.Column(db.String(80), nullable=False)
+	description = db.Column(db.Text)
+	price = db.Column(db.Numeric(precision=10, scale=2))
+	timestamp = db.Column(db.DateTime, index=True, 
+							default=datetime.utcnow)
+	category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
