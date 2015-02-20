194b8bdd80a6e0704e416aca9cdf721cd8e97dcc
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-19 19:09:28 -0800

    added bootstrap extension

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -1,15 +1,15 @@
 from flask import Flask
-#from flask.ext.bootstrap import Bootstrap
+from flask.ext.bootstrap import Bootstrap
 from config import config
 
-#bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
-    #bootstrap.init_app(app)
+    bootstrap.init_app(app)
     
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
