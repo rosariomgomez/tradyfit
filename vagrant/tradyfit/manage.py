1ee2d27c0e345a006b13349d9fe9b533af25e7cc
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 11:37:02 -0800

    trying to solve reading tests from drone.io

--- vagrant/tradyfit/manage.py
+++ vagrant/tradyfit/manage.py
@@ -20,7 +20,7 @@ manager.add_command('db', MigrateCommand)
 def test():
     """Run the unit tests."""
     import unittest
-    tests = unittest.TestLoader().discover('tests')
+    tests = unittest.TestLoader().discover('./tests/')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
 
