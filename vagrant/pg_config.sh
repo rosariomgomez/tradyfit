55b2d0b03c9798b423d66e1c086bd7854d92d5f0
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-19 18:30:42 -0800

    installed tree tool

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -1,5 +1,6 @@
 
 apt-get -qqy update
+apt-get -qqy install tree
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
