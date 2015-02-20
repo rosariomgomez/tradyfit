0209a46dcf482f441dc277007b7548a4d2569e72
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 11:07:18 -0800

    imported Category and Item models

--- vagrant/tradyfit/manage.py
+++ vagrant/tradyfit/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 from app import create_app, db
+from app.models import Category, Item
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
@@ -10,7 +11,7 @@ migrate = Migrate(app, db)
 
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Category=Category, Item=Item)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
