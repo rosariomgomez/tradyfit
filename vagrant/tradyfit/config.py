4872aec8dc63bff4e40e709ed506d42c7c9e3745
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 07:49:39 -0800

    added connection to postgres db in dev and test configs

--- vagrant/tradyfit/config.py
+++ vagrant/tradyfit/config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'our so secret password'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     TRADYFIT_ADMIN = os.environ.get('TRADYFIT_ADMIN')
 
     @staticmethod
@@ -14,10 +15,14 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'postgresql:///tradyfit_dev'
     
 
 class TestingConfig(Config):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'postgresql:///tradyfit_test'
     WTF_CSRF_ENABLED = False #disable csrf token protection on forms
 
 
