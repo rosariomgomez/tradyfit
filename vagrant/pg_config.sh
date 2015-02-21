4b4de89001a6861299fb11602517b69436499fbc
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-20 20:46:05 -0800

    changed provisioning script to same syntax I use in drone.io

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -4,5 +4,6 @@ apt-get -qqy install tree
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
-su postgres -c 'createuser -dRS vagrant'
-su vagrant -c 'createdb'
+createuser -dRS vagrant -U postgres
+createdb tradyfit_dev -U vagrant
+createdb tradyfit_test -U vagrant
