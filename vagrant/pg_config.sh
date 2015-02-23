eaae2096c51246c9a78c427861992201660ce5d1
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-23 12:01:59 -0800

    VM provision failed, so rolled back to previous set up

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -4,6 +4,6 @@ apt-get -qqy install tree
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-flask python-sqlalchemy
 apt-get -qqy install python-pip
-createuser -dRS vagrant -U postgres
-createdb tradyfit_dev -U vagrant
-createdb tradyfit_test -U vagrant
+su postgres -c 'createuser -dRS vagrant'
+su vagrant -c 'createdb tradyfit_dev'
+su vagrant -c 'createdb tradyfit_test'
