e95e85dcf655a60a50cf868ff7f4609aaf8009a9
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-26 12:10:04 -0800

    added ItemForm class

---
+++ vagrant/tradyfit/app/main/forms.py
@@ -0,0 +1,24 @@
+from flask.ext.wtf import Form
+from wtforms import StringField, SelectField, SubmitField, \
+TextAreaField, DecimalField, ValidationError
+from wtforms.validators import Required, Length, NumberRange, Regexp
+from ..models import Category, Item
+
+
+class ItemForm(Form):
+
+	name = 	StringField('Name', 
+						validators = [ Required(), Length(3, 80), 
+								   Regexp('^[A-Za-z][. \w]+$', 0,
+								   'Product names must have only letters,'
+								   ' numbers, dots or underscores')])
+	description = 	TextAreaField('Description', 
+									validators=[Length(0,500)])
+	price = DecimalField('Price', default=0, places=2, 
+						 validators=[NumberRange(0, 10**10-1)])
+	category = 	SelectField('Category', coerce=int)
+	submit = SubmitField('Submit')
+
+	def __init__(self, *args, **kwargs):
+		super(ItemForm, self).__init__(*args, **kwargs)
+		self.category.choices = Category.get_category_choices()
