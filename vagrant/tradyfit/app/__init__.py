165bc6e522e8cc9cae12f71fb3b58a1b0da153fe
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-27 10:49:11 -0800

    initialize flask-moment

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.moment import Moment
 from config import config
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+moment = Moment()
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,7 +15,8 @@ def create_app(config_name):
 
     bootstrap.init_app(app)
     db.init_app(app)
-    
+    moment.init_app(app)
+
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
