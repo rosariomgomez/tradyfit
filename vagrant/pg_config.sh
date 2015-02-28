b16db58d823a61ebf4f8d6e2f1415d833d8b2a6e
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-28 11:15:20 -0800

    removed python-sqlalchemy

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -3,7 +3,7 @@ apt-get -qqy update
 apt-get -qqy install tree
 apt-get -qqy install postgresql python-psycopg2
 apt-get -qqy install python-dev
-apt-get -qqy install python-flask python-sqlalchemy
+apt-get -qqy install python-flask
 apt-get -qqy install python-pip
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb tradyfit_dev'
