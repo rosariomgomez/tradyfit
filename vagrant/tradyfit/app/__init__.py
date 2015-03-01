b643f62ad7ffa25f65a19553bb3198c5c5830e82
Rosario Martinez <rosario.mgomez@gmail.com>
2015-02-28 23:30:52 -0800

    included auth blueprint and initialize login_manager

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
 from config import config
 
@@ -8,16 +9,23 @@ bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
 
+login_manager = LoginManager()
+login_manager.login_view = 'main.index'
+
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
     bootstrap.init_app(app)
-    db.init_app(app)
     moment.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
 
     return app
