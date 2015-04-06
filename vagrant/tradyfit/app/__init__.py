08bca2943cde4ed5348ec15e7a824b2ff97501a6
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-05 20:21:27 -0700

    fixed indentation

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -13,19 +13,19 @@ login_manager = LoginManager()
 login_manager.login_view = 'main.index'
 
 def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
+  app = Flask(__name__)
+  app.config.from_object(config[config_name])
+  config[config_name].init_app(app)
 
-    bootstrap.init_app(app)
-    moment.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
+  bootstrap.init_app(app)
+  moment.init_app(app)
+  db.init_app(app)
+  login_manager.init_app(app)
 
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint)
+  from .auth import auth as auth_blueprint
+  app.register_blueprint(auth_blueprint)
 
-    return app
+  return app
