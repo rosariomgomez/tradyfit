400d3c58599709573701085452e1ad47f91ac51f
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-08 19:50:49 -0700

    added image FileField to ItemForm, allow names start with numbers

--- vagrant/tradyfit/app/main/forms.py
+++ vagrant/tradyfit/app/main/forms.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from flask.ext.wtf import Form
 from wtforms import StringField, SelectField, SubmitField, \
-TextAreaField, DecimalField, ValidationError
+TextAreaField, DecimalField
+from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import Required, Length, NumberRange, Regexp
 from ..models import Category, Item
 
@@ -9,13 +10,16 @@ from ..models import Category, Item
 class ItemForm(Form):
   name = StringField('Name', validators =
                                 [ Required(), Length(3, 80),
-                                  Regexp('^[A-Za-z][.\- \w]+$', 0,
+                                  Regexp('^[A-Za-z0-9][.\- \w]+$', 0,
                                   'Product names must have only letters,'
                                   ' numbers, dots, dashes or underscores')])
   description = TextAreaField('Description', validators=[Length(0,500)])
   price = DecimalField('Price', default=0, places=2,
                         validators= [NumberRange(0, 10**10-1)])
   category = SelectField('Category', coerce=int)
+  image = FileField('Image', validators=[
+                    FileAllowed(['jpg', 'jpeg', 'png', 'gif'],
+                      'Only jpg, png and gif files allowed')])
   submit = SubmitField('Submit')
 
   def __init__(self, *args, **kwargs):
