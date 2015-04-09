f8c8028722e6af3428af4d4fafd09938f300c168
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-08 19:47:46 -0700

    added error handler for 413 exception

--- vagrant/tradyfit/app/main/errors.py
+++ vagrant/tradyfit/app/main/errors.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from . import main
+
+
+@main.app_errorhandler(413)
+def request_entity_too_large(e):
+  '''redirect to create item form if a user tries to upload
+  an image bigger than 3MB'''
+  flash('Image should be less than 3MB.')
+  return redirect(url_for('main.create'))
 
 
 @main.app_errorhandler(404)
