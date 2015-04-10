5495fc99ee95d2acd94a9a5e1bce45409e0e3f7d
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-10 16:00:08 -0700

    added integration tests for the home page

--- vagrant/tradyfit/tests/integration/test_index.py
+++ vagrant/tradyfit/tests/integration/test_index.py
@@ -3,16 +3,21 @@ import unittest
 import re
 from uuid import uuid4
 from bs4 import BeautifulSoup
+from mock import Mock, patch
 from flask import current_app, url_for
 from app import create_app, db
 from app.models import Item, Category, User
+import app.main.views
 
 
-class IndexFunctionalTestCase(unittest.TestCase):
+class IndexIntegrationTestCase(unittest.TestCase):
   def setUp(self):
     self.app = create_app('testing')
     self.app_context = self.app.app_context()
     self.app_context.push()
+    #do not request urls from S3
+    self.app.config["S3_LOCATION"] = ''
+
     db.create_all()
     Category.insert_categories()
     self.client = self.app.test_client()
@@ -22,35 +27,16 @@ class IndexFunctionalTestCase(unittest.TestCase):
     db.drop_all()
     self.app_context.pop()
 
-  def test_edit_link(self):
-    '''verify an item can be edited from the index page by the owner
-    1. Create two items by two different users
-    2. Go to index page
-    3. Assert the edit link is present for the user's item
-    '''
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url=uuid4().hex + '.jpg')
-    u1 = User(fb_id='25', email='maggy@example.com', name='Maggy Simpson',
-              username='maggy', avatar_url=uuid4().hex + '.jpg')
-    db.session.add_all([u,u1])
-    db.session.commit()
-    c = Category.query.filter_by(name='soccer').one()
-    item = Item(name='soccer ball', description='plain ball',
-        price=23, category=c, user_id=u.id, image_url='item.jpg')
-    item2 = Item(name='soccer t-shirt', description='Real Madrid size M',
-        price=28, category=c, user_id=u1.id, image_url='item2.jpg')
-    db.session.add_all([item,item2])
-    db.session.commit()
-    with self.client as c:
-      with c.session_transaction() as sess:
-        sess['user_id'] = u.id
-        sess['_fresh'] = True
-      response = self.client.get(url_for('main.index'))
-      r = response.get_data(as_text=True)
-      self.assertTrue('<a href="/edit/' + str(item.id) + '"' in r)
-      self.assertFalse('<a href="/edit/' + str(item2.id) + '"' in r)
+  def test_search_form_redirect(self):
+    '''verify the search form redirects to results when submit'''
+    resp = self.client.post(url_for('main.index'),
+                            data={
+                                'search': 'soccer ball',
+                            }, follow_redirects=True)
+    self.assertTrue(b'Search results for "soccer ball":' in resp.data)
 
-  def test_delete_item(self):
+  @patch('app.main.views.delete_item_image')
+  def test_delete_item(self, mock_delete_image):
     '''verify an item can be deleted from the index page
     1. Create an item
     2. Go to index page
@@ -72,6 +58,10 @@ class IndexFunctionalTestCase(unittest.TestCase):
         price=28, category=c, user_id=u1.id, image_url='item2.jpg')
     db.session.add_all([item,item2])
     db.session.commit()
+
+    #mock the call to delete image
+    mock_delete_image.return_value = True
+
     with self.client as c:
       with c.session_transaction() as sess:
         sess['user_id'] = u.id
@@ -84,4 +74,3 @@ class IndexFunctionalTestCase(unittest.TestCase):
                                 follow_redirects=True)
       r = response.get_data(as_text=True)
       self.assertFalse(item.name in r)
-
