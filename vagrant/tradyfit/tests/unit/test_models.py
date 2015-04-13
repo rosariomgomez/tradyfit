09e5b5db61c32255509bbd3f2d6b921fd9a5dc00
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-13 14:59:50 -0700

    moved user creation

--- vagrant/tradyfit/tests/unit/test_models.py
+++ vagrant/tradyfit/tests/unit/test_models.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
 import time
 from mock import patch
-from base import UnitTestCase
+from base import BasicTestCase, UnitTestCase
 from app import db
-from app.models import Category, Item, User, load_user
+from app.models import Category, User, load_user
 from app.geolocation import Geolocation
 
 
-class CategoryModelTestCase(UnitTestCase):
+class CategoryModelTestCase(BasicTestCase):
+
+  def setUp(self):
+    super(CategoryModelTestCase, self).setUp()
+    db.create_all()
+
+  def tearDown(self):
+    db.session.remove()
+    db.drop_all()
+    super(CategoryModelTestCase, self).tearDown()
+
   def test_insert_categories(self):
     categories = Category.query.all()
     self.assertEqual(categories, [])
     Category.insert_categories()
-    c = Category.query.filter_by(name='soccer').one()
+    c = Category.get_category(name='soccer')
     self.assertEqual(c.name, 'soccer')
 
   def test_insert_categories_no_dup(self):
@@ -26,56 +36,39 @@ class CategoryModelTestCase(UnitTestCase):
 
 
 class UserModelTestCase(UnitTestCase):
+
   def test_username(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     #the username already exists, append the next uid to the name
     self.assertTrue(User.create_username('john') == 'john2')
     #username doesn't exist, so it can be assigned
     self.assertTrue(User.create_username('jacky') == 'jacky')
 
   def test_ping(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     time.sleep(2)
     last_seen_before = u.last_seen
     u.ping()
     self.assertTrue(u.last_seen > last_seen_before)
 
   def test_get_user(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     self.assertEqual(User.get_user('john@example.com'), u)
     self.assertEqual(User.get_user('nobody@example.com'), None)
 
   def test_load_user(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     self.assertEqual(load_user(u.id), u)
 
   def test_get_avatar(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     avatar = self.app.config['S3_LOCATION'] + "/" + \
                 self.app.config['S3_BUCKET'] + \
                 self.app.config['S3_UPLOAD_AVATAR_DIR'] + "/" + u.avatar_url
     self.assertEqual(u.avatar(), avatar)
 
   def test_location_user_with_city(self):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg', city='Mountain View')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
     u.location('10.0.0.2')
     self.assertTrue(u.latitude == None)
 
@@ -87,30 +80,24 @@ class UserModelTestCase(UnitTestCase):
   @patch('app.geolocation.Geolocation.get_longitude', return_value=(True, -98))
   def test_location_user_no_city(self, m_geo, m_country, m_state, m_city,
       m_latitude, m_longitude):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
+    u.city = ''
     u.location('10.0.0.2')
     self.assertTrue(u.latitude == 123)
 
   @patch('app.geolocation.Geolocation', return_value={'key': 'value'})
   @patch('app.geolocation.Geolocation.get_country', return_value=(True, ''))
   def test_location_user_no_city_no_country(self, m_geo, m_country):
-    u = User(fb_id='23', email='john@example.com', name='John Doe',
-            username='john', avatar_url='avatar.jpg')
-    db.session.add(u)
-    db.session.commit()
+    u = self.create_user()
+    u.city = ''
     u.location('10.0.0.2')
     self.assertTrue(u.latitude == None)
 
 
 class ItemModelTestCase(UnitTestCase):
   def test_get_image(self):
-    item = Item(name='ball', description='small and round', price=23,
-                image_url='ball.jpg')
-    db.session.add(item)
-    db.session.commit()
+    user = self.create_user()
+    item = self.create_item(user.id)
     image = self.app.config['S3_LOCATION'] + "/" + \
                 self.app.config['S3_BUCKET'] + \
                 self.app.config['S3_UPLOAD_ITEM_DIR'] + "/" + item.image_url
