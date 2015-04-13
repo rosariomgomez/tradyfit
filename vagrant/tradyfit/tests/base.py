8356ee1e1f49b1107525204d16ab0cdccd2d44f3
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-13 14:57:18 -0700

    added methods to create user and item

--- vagrant/tradyfit/tests/base.py
+++ vagrant/tradyfit/tests/base.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from app import create_app, db
-from app.models import Category
+from app.models import Category, User, Item
 
 
 class BasicTestCase(unittest.TestCase):
@@ -20,18 +20,35 @@ class UnitTestCase(BasicTestCase):
   def setUp(self):
     super(UnitTestCase, self).setUp()
     db.create_all()
+    Category.insert_categories()
 
   def tearDown(self):
     db.session.remove()
     db.drop_all()
     super(UnitTestCase, self).tearDown()
 
+  def create_user(self, fb_id='23', email='john@example.com', username='john'):
+    u = User(fb_id=fb_id, email=email, name='John Doe', username=username,
+            avatar_url='avatar.jpg', city='Mountain View')
+    db.session.add(u)
+    db.session.commit()
+    return u
+
+  def create_item(self, user_id, item_name='ball', item_desc='small and round'):
+    c = Category.get_category('soccer')
+    item = Item(name=item_name, description=item_desc, price=23,
+                category_id=c.id, image_url='ball.jpg', user_id=user_id)
+    db.session.add(item)
+    db.session.commit()
+    return item
+
 
 class ClientTestCase(UnitTestCase):
 
   def setUp(self):
     super(ClientTestCase, self).setUp()
-    self.client = self.app.test_client()
     Category.insert_categories()
+    self.client = self.app.test_client()
     #do not request urls from S3
     self.app.config["S3_LOCATION"] = ''
+
