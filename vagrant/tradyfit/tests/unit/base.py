9a90d3c173dc272be4b10ac6662d71a3fcb6cea6
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-13 10:43:27 -0700

    added class for client requests

--- vagrant/tradyfit/tests/unit/base.py
+++ vagrant/tradyfit/tests/unit/base.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import unittest
 from app import create_app, db
+from app.models import Category
 
 
 class BasicTestCase(unittest.TestCase):
@@ -24,3 +25,13 @@ class UnitTestCase(BasicTestCase):
     db.session.remove()
     db.drop_all()
     super(UnitTestCase, self).tearDown()
+
+
+class ClientTestCase(UnitTestCase):
+
+  def setUp(self):
+    super(ClientTestCase, self).setUp()
+    self.client = self.app.test_client()
+    Category.insert_categories()
+    #do not request urls from S3
+    self.app.config["S3_LOCATION"] = ''
