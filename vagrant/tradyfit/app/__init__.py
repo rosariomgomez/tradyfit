1cce74423ac9277e022a2e0089e15d051857c72e
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-13 22:29:55 -0700

    added geonamescache to import Countries and States

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -4,10 +4,13 @@ from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
 from config import config
+import geonamescache
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 moment = Moment()
+gc = geonamescache.GeonamesCache()
 
 login_manager = LoginManager()
 login_manager.login_view = 'main.index'
