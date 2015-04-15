2b669543d7b9b6148f3e342e90153f0f8b0e0a4d
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-15 14:06:21 -0700

    added tests for MessageForm

--- vagrant/tradyfit/tests/unit/main/test_forms.py
+++ vagrant/tradyfit/tests/unit/main/test_forms.py
@@ -4,11 +4,13 @@ from werkzeug import FileStorage
 from mock import Mock
 from base import UnitTestCase
 from app.models import Item, Category
-from app.main.forms import UserForm, ItemForm, SearchForm
+from app.main.forms import UserForm, ItemForm, SearchForm, MessageForm
 
 
 LONG_TEXT = 'The Believers. The hidden story behind the code that runs our \
-lives and control everything by Geoffrey Hinton'
+lives and control everything. Geoffrey Hinton. A believer. A brusque coder \
+who had to hide his ideas in obscure language to get them past peer review. \
+A guru of the artificial neural network.'
 
 def create_user_form(user, username, name, country, state, city):
   return UserForm(user=user, data={
@@ -317,4 +319,43 @@ class SearchFormTestCase(UnitTestCase):
     self.assertEqual(form.errors['search'], [u'Field must be between 3 and' +
                                             ' 80 characters long.'])
 
+class MessageFormTestCase(UnitTestCase):
+  def test_message_form(self):
+    '''verify a the form validates with valid inputs'''
+    form = MessageForm(data={'subject': 'Hi there!', 'description': 'Nice'})
+    form.validate()
+    self.assertTrue(not form.errors)
 
+  def test_form_subject_field(self):
+    '''verify that subject field only accepts the defined data types'''
+
+    #1. TC1: Empty subject
+    form = MessageForm(data={'subject': '', 'description': 'Nice'})
+    form.validate()
+    self.assertEqual(form.errors['subject'], ['This field is required.'])
+
+    #2. TC2: Short (<2 chars)
+    form = MessageForm(data={'subject': 'f', 'description': 'Nice'})
+    form.validate()
+    self.assertEqual(form.errors['subject'], ['Field must be between 2 and' +
+                                            ' 120 characters long.'])
+
+    #3. TC3: Long (>120 chars)
+    form = MessageForm(data={'subject': LONG_TEXT, 'description': 'Nice'})
+    form.validate()
+    self.assertEqual(form.errors['subject'], ['Field must be between 2 and' +
+                                            ' 120 characters long.'])
+
+  def test_form_description_field(self):
+    '''verify that description field only accepts the defined data types'''
+
+    #1. TC1: Long (>500 chars)
+    path = os.path.dirname(os.path.abspath(__file__))
+    f = os.path.join(path,"sample_text.txt")
+    with open(f, "r") as text:
+        desc = text.readlines()
+
+    form = MessageForm(data={'subject': 'hi', 'description': desc[0]})
+    form.validate()
+    self.assertEqual(form.errors['description'], [u'Field must be ' +
+                                'between 0 and 500 characters long.'])
