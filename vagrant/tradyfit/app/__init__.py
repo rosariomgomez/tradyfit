a29e2ac30fd65c8931f40cd2bb45c5b7d21d54b8
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-16 16:28:11 -0700

    instantiate JSGlue

--- vagrant/tradyfit/app/__init__.py
+++ vagrant/tradyfit/app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
+from flask_jsglue import JSGlue
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.moment import Moment
@@ -13,6 +14,7 @@ db = SQLAlchemy()
 moment = Moment()
 gc = geonamescache.GeonamesCache()
 geolocator = GoogleV3()
+jsglue = JSGlue()
 
 login_manager = LoginManager()
 login_manager.login_view = 'main.index'
@@ -26,6 +28,7 @@ def create_app(config_name):
   moment.init_app(app)
   db.init_app(app)
   login_manager.init_app(app)
+  jsglue.init_app(app)
 
   from .main import main as main_blueprint
   app.register_blueprint(main_blueprint)
