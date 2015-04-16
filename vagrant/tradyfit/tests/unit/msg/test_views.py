fee992900d0152ebda75575ed0a175e28384d3a6
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-15 17:31:45 -0700

    added test for notifications route

--- vagrant/tradyfit/tests/unit/msg/test_views.py
+++ vagrant/tradyfit/tests/unit/msg/test_views.py
@@ -5,7 +5,7 @@ import app.msg.views
 
 
 class CreateViewTestCase(ClientTestCase):
-  '''Testing: @msg.route('/create/<int:id>', methods=['GET', 'POST'])'''
+  '''Testing: @msg.route('msg/create/<int:id>', methods=['GET', 'POST'])'''
 
   def test_create_message_route(self):
     '''verify you can go to create message page if an existent item is passed
@@ -31,3 +31,14 @@ class CreateViewTestCase(ClientTestCase):
       #2. create message page
       response = self.client.get(url_for('msg.create', id=item.id))
       self.assertEquals(response.status_code, 200)
+
+
+class NotificationsViewTestCase(ClientTestCase):
+  '''Testing: @msg.route('/notifications')'''
+
+  def test_notifiactions_route(self):
+    '''verify you can go to notifications page'''
+    user = self.create_user()
+    response = self.make_get_request(user, 'msg.notifications')
+    self.assertEquals(response.status_code, 200)
+    self.assertTrue('msgs-unread' in response.data)
