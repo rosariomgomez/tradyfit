5dbcb3b301161921d6260e19c4671df97a43853d
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-17 16:20:18 -0700

    added postgis support for psql and created extension

--- vagrant/pg_config.sh
+++ vagrant/pg_config.sh
@@ -2,9 +2,12 @@
 apt-get -qqy update
 apt-get -qqy install tree
 apt-get -qqy install postgresql python-psycopg2
+apt-get install postgis postgresql-9.3-postgis-2.1
 apt-get -qqy install python-dev
 apt-get -qqy install python-flask
 apt-get -qqy install python-pip
 su postgres -c 'createuser -dRS vagrant'
 su vagrant -c 'createdb tradyfit_dev'
 su vagrant -c 'createdb tradyfit_test'
+su vagrant psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" tradyfit_dev
+su vagrant psql -c "CREATE EXTENSION postgis; CREATE EXTENSION postgis_topology;" tradyfit_test
