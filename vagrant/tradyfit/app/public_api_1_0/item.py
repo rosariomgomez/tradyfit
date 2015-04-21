b1623bb3c61edd912a785e209f7e304fd0437333
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-20 19:18:21 -0700

    closes #6

--- vagrant/tradyfit/app/public_api_1_0/item.py
+++ vagrant/tradyfit/app/public_api_1_0/item.py
@@ -1,7 +1,18 @@
 # -*- coding: utf-8 -*-
+import re
 from flask import jsonify, request, url_for
 from ..models import Category, Item
 from . import public_api
+from .errors import bad_request
+
+MIN_QUERY = 3
+MAX_QUERY = 80
+
+def valid_query(query):
+  '''query should be between (3,80) start with letter or numbers and only 
+  include letters, numbers, spaces, "." or "-" '''
+  return MIN_QUERY < len(query) < MAX_QUERY and \
+          bool(re.match("^[A-Za-z0-9][.\- \w]+$", query))
 
 
 @public_api.route('/items/category/<category>')
@@ -10,3 +21,14 @@ def get_items_category(category):
   items = Item.query.filter_by(category_id = category.id).order_by(
                   Item.timestamp.desc()).all()
   return jsonify(items=[item.serialize for item in items])
+
+
+@public_api.route('/items/search/<query>')
+def get_items_search(query):
+  '''return items containing the query text on name or description'''
+  if valid_query(query):
+    items = Item.query.search(query).order_by(
+                    Item.timestamp.desc()).all()
+    return jsonify(items=[item.serialize for item in items])
+  else:
+    return bad_request('Not a valid search')
--- vagrant/tradyfit/tests/unit/public_api_1_0/test_api.py
+++ vagrant/tradyfit/tests/unit/public_api_1_0/test_api.py
@@ -19,7 +19,8 @@ class APITestCase(ClientTestCase):
     self.assertTrue(json_response['error'] == 'not found')
 
   def test_get_items_invalid_category(self):
-    '''request items for a non existent category.
+    '''testing @public_api.route('/items/category/<category>')
+    request items for a non existent category.
     Verify you get a 404
     '''
     response = self.client.get('public-api/v1.0/items/category/notacategory', 
@@ -28,7 +29,7 @@ class APITestCase(ClientTestCase):
 
 
   def test_get_items_valid_category(self):
-    '''testing @public_api.route('/items/<category>')
+    '''testing @public_api.route('/items/category/<category>')
     1. Create 2 items. One from soccer and other from cycling category
     2. Request items from soccer category
     3. Verify only the item from soccer was returned'''
@@ -42,5 +43,42 @@ class APITestCase(ClientTestCase):
     json_response = json.loads(response.data.decode('utf-8'))
     self.assertTrue(len(json_response['items']) == 1)
     self.assertTrue(json_response['items'][0]['name'] == item2.name)
+
+
+  def test_get_items_invalid_search(self):
+    '''testing @public_api.route('/items/search/<search>')
+    Verify you get a 400 responses when query is not valid'''
+    #1. request with query < MIN_QUERY
+    response = self.client.get('public-api/v1.0/items/search/fo', 
+                headers=self.get_api_headers())
+    self.assertTrue(response.status_code == 400)
+
+    #2. request with query starting with "-"
+    response = self.client.get('public-api/v1.0/items/search/-something', 
+                headers=self.get_api_headers())
+    self.assertTrue(response.status_code == 400)
+
+    #3. request with query containing special chars
+    response = self.client.get('public-api/v1.0/items/search/<script>hi', 
+                headers=self.get_api_headers())
+    self.assertTrue(response.status_code == 400)
+
+
+  def test_get_items_valid_search(self):
+    '''testing @public_api.route('/items/search/<search>')
+    1. Create 3 items. Two of them containing the words "blue bike"
+    2. Request items with the query="blue bike"
+    3. Verify the two items were returned ordered by desc timestamp'''
+
+    user = self.create_user_location()
+    item1 = self.create_item(user.id)
+    item2 = self.create_item(user.id, 'bike', 'blue and red', 'cycling')
+    item3 = self.create_item(user.id, 'tri bike', 'blue print', 'cycling')
+    response = self.client.get('public-api/v1.0/items/search/blue bike', 
+                headers=self.get_api_headers())
+    self.assertTrue(response.status_code == 200)
+    json_response = json.loads(response.data.decode('utf-8'))
+    self.assertTrue(len(json_response['items']) == 2)
+    self.assertTrue(json_response['items'][0]['name'] == item3.name)
 
 
