65afbdf0df43f424e2298ccb6948423de2be81bb
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-20 17:53:03 -0700

    added get request for items with category

---
+++ vagrant/tradyfit/app/public_api_1_0/item.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from flask import jsonify, request, url_for
+from ..models import Category, Item
+from . import public_api
+
+
+@public_api.route('/items/<category>')
+def get_items(category):
+  category = Category.query.filter_by(name=category).first_or_404()
+  items = Item.query.filter_by(category_id = category.id).order_by(
+                  Item.timestamp.desc()).all()
+  return jsonify(items=[item.serialize for item in items])
