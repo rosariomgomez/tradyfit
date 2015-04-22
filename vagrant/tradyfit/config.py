3d779f01669cc74ae20eea01bbe8f2424ba0744a
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-22 12:46:28 -0700

    specified acces token method

--- vagrant/tradyfit/config.py
+++ vagrant/tradyfit/config.py
@@ -16,6 +16,7 @@ class Config:
     'base_url': 'https://graph.facebook.com',
     'request_token_url': None,
     'access_token_url': '/oauth/access_token',
+    'access_token_method': 'GET',
     'authorize_url': 'https://www.facebook.com/dialog/oauth'
   }
 
