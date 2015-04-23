a5ff4d948c4c378a3442e136d9e56de3d7ee2224
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-22 17:38:52 -0700

    refactor and added exception values

--- vagrant/tradyfit/app/geolocation.py
+++ vagrant/tradyfit/app/geolocation.py
@@ -29,19 +29,22 @@ class Geolocation(object):
   @staticmethod
   def get_location(ip):
     '''use the MaxMind DB to find the user location from the IP address.
-    maxminddb internally verify if it is a valid IP
+    maxminddb internally verify if it is a valid IP.
+    If an error occur (not a valid IP) an exception is raised. If there is no
+    info for that ip e.g. 127.0.0.1, None is also the returned value
     Input: (string) ip
-    Output: (dict) geolocation info or None if an error occur'''
+    Output: (dict) geolocation info or None
+    '''
     try:
       return geo_db.get(ip)
-    except:
+    except ValueError:
       return None
 
 
   @staticmethod
   def create_address(city, state, country):
     '''generate a string with an address'''
-    if country == 'US':
+    if country == 'US' and state != 'NU': #'NU'== 'Not US'
       return ', '.join([city, state, country])
     else:
       return ', '.join([city, country])
@@ -64,29 +67,39 @@ class Geolocation(object):
   def get_city(self):
     '''return city from dict geolocation.
     Even that get the city doesn't throws an exception, it could be None
-    Possible Outputs: (True, <string>city or None) or (False, None)
+    Possible Outputs: (True, <string>city) or (False, None)
     '''
     try:
       city = self.location.get('city').get('names').get('en')
-      return True, city
-    except:
+      if city:
+        return True, city
+      return False, None
+    except AttributeError:
+      return False, None
+
+
+  def get_location_value(self, param1, param2):
+    '''helper method to retrieve country, latitude or longitude values
+    Possible Outputs: (True, <string>) or (False, None)'''
+    try:
+      value = self.location.get(param1).get(param2)
+      if value:
+        return True, value
+      return False, None
+    except AttributeError:
       return False, None
 
 
   def get_country(self):
     '''return country from dict geolocation
-    Possible Outputs: (True, <string>city or None) or (False, None)
+    Possible Outputs: (True, <string>country) or (False, None)
     '''
-    try:
-      country = self.location.get('country').get('iso_code')
-      return True, country
-    except:
-      return False, None
+    return self.get_location_value('country', 'iso_code')
 
 
   def get_state(self):
     '''return state from dict geolocation if country is US
-    Possible Outputs: (True, <string>city or None) or (False, None)
+    Possible Outputs: (True, <string>state) or (False, None)
     '''
     r, country = self.get_country()
     if r and country == 'US':
@@ -95,7 +108,7 @@ class Geolocation(object):
         if state:
           state = state[0].get('iso_code')
         return True, state
-      except:
+      except AttributeError:
         return False, None
     else:
       return False, None
@@ -103,22 +116,14 @@ class Geolocation(object):
 
   def get_latitude(self):
     '''return latitude from dict geolocation
-    Possible Outputs: (True, <string>city or None) or (False, None)
+    Possible Outputs: (True, <string>latitude) or (False, None)
     '''
-    try:
-      latitude = self.location.get('location').get('latitude')
-      return True, latitude
-    except:
-      return False, None
+    return self.get_location_value('location', 'latitude')
 
 
   def get_longitude(self):
     '''return longitude from dict geolocation
-    Possible Outputs: (True, <string>city or None) or (False, None)
+    Possible Outputs: (True, <string>longitude) or (False, None)
     '''
-    try:
-      longitude = self.location.get('location').get('longitude')
-      return True, longitude
-    except:
-      return False, None
+    return self.get_location_value('location', 'longitude')
 
