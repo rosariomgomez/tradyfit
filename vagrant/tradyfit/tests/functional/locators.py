fa39c16e9d9a4c963635fe9fd60edb83285afc66
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-27 11:44:54 -0700

    added locators for navbar

--- vagrant/tradyfit/tests/functional/locators.py
+++ vagrant/tradyfit/tests/functional/locators.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
 from selenium.webdriver.common.by import By
+
 
 class NavBarLocators(object):
   LOGIN = (By.LINK_TEXT, 'Log In with Facebook')
   LIST_ITEM = (By.LINK_TEXT, 'List an item')
+  BROWSE_CATEGORIES = (By.LINK_TEXT, 'Browse categories')
+  NOTIFICATIONS = (By.LINK_TEXT, 'Notifications')
+  USER_DROPDOWN = (By.ID, 'user-dropdown')
+  PROFILE = (By.LINK_TEXT, 'Profile')
+  LOGOUT = (By.LINK_TEXT, 'Log Out')
 
 class LoginPageLocators(object):
   EMAIL = (By.NAME, 'email')
