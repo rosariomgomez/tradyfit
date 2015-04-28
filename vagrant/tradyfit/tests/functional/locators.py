782face62df9b310c071af4b8f1ce508b010da56
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-28 12:35:26 -0700

    added new locators #30

--- vagrant/tradyfit/tests/functional/locators.py
+++ vagrant/tradyfit/tests/functional/locators.py
@@ -3,17 +3,30 @@ from selenium.webdriver.common.by import By
 
 
 class NavBarLocators(object):
+  HOME = (By.LINK_TEXT, 'TradyFit')
   LOGIN = (By.LINK_TEXT, 'Log In with Facebook')
   LIST_ITEM = (By.LINK_TEXT, 'List an item')
   BROWSE_CATEGORIES = (By.LINK_TEXT, 'Browse categories')
-  NOTIFICATIONS = (By.LINK_TEXT, 'Notifications')
+  NOTIFICATIONS = (By.ID, 'notif-link')
+  NOTIFICATIONS_BADGE = (By.CLASS_NAME, 'badge')
   USER_DROPDOWN = (By.ID, 'user-dropdown')
   PROFILE = (By.LINK_TEXT, 'Profile')
   LOGOUT = (By.LINK_TEXT, 'Log Out')
 
+
 class LoginPageLocators(object):
   EMAIL = (By.NAME, 'email')
   PASS = (By.NAME, 'pass')
+
+
+class HomePageLocators(object):
+  SEARCH = (By.ID, 'search')
+  SUBMIT = (By.ID, 'submit-search')
+
+
+class ItemPageLocators(object):
+  CONTACT = (By.LINK_TEXT, 'Contact seller')
+
 
 class ListItemPageLocators(object):
   NAME = (By.ID, 'name')
@@ -22,3 +35,20 @@ class ListItemPageLocators(object):
   CATEGORY = (By.ID, 'category')
   IMAGE = (By.ID, 'image')
   SUBMIT = (By.ID, 'submit')
+
+
+class CreateMessagePageLocators(object):
+  DESCRIPTION = (By.ID, 'description')
+  SEND = (By.ID, 'submit')
+
+
+class MessagePageLocators(object):
+  DESCRIPTION = (By.ID, 'description')
+  SEND = (By.ID, 'submit')
+
+
+class NotificationsPageLocators(object):
+  UNREAD = (By.CLASS_NAME, 'num-unread')
+  SENT = (By.CLASS_NAME, 'num-sent')
+  RECEIVED = (By.CLASS_NAME, 'num-sent')
+
