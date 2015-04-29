bc04220efa143f27ef97c8bd44dfae4a747622cd
Rosario Martinez <rosario.mgomez@gmail.com>
2015-04-29 12:55:53 -0700

    added deploy method #33

--- vagrant/tradyfit/manage.py
+++ vagrant/tradyfit/manage.py
@@ -61,5 +61,17 @@ def acceptance_test():
   unittest.TextTestRunner(verbosity=2).run(tests)
 
 
+@manager.command
+def deploy():
+  """Run deployment tasks."""
+  from flask.ext.migrate import upgrade
+  from app.models import Category
+
+  # migrate database to latest revision
+  upgrade()
+  # create categories
+  Category.insert_categories()
+
+
 if __name__ == '__main__':
   manager.run()
