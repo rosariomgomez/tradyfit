65f03fdd866124d3f0feb68ca1a89611f1a5798b
Rosario Martinez <rosario.mgomez@gmail.com>
2015-05-01 10:46:14 -0700

    missing quote on regexp

--- vagrant/tradyfit/app/msg/forms.py
+++ vagrant/tradyfit/app/msg/forms.py
@@ -6,7 +6,7 @@ from wtforms.validators import Required, Length, Regexp
 
 class MessageForm(Form):
   subject = TextAreaField('Subject (*)', validators=[Required(), Length(3,120),
-                            Regexp('^[A-Za-z0-9][.\-\:\! \w]+$', 0,
+                            Regexp('^[A-Za-z0-9][.\-\:\!\' \w]+$', 0,
                                   'Subject must have only letters,'
                                   ' numbers, dots, dashes or underscores')])
   description = TextAreaField('Description', validators=[Length(0,500)])
